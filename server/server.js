bc98661d72eb378d9ad974ca7d936c1ffe8d5766
jnsw <jns.w@icloud.com>
2020-12-12 10:23:57 +0800

    all

---
+++ server/server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const passport = require('./configs/passport.config');
+
+const port = process.env.PORT;
+const app = express();
+
+require('./configs/mongo.config');
+
+app.use(express.urlencoded({ extended: true }));
+app.use('/', express.static('dist'));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.json());
+
+app.use('/api', require('./routes/index.routes'));
+
+app.get('*', (req, res) => {
+  res.sendStatus(404);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
