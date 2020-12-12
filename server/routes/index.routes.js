c1f168bdea1820f35b6772e6220ba43977b91e75
jnsw <jns.w@icloud.com>
2020-12-12 10:17:30 +0800

    Customer Frontend Updates.

--- server/routes/index.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-router.use('/auth', require('./auth.routes'));
-router.use('/categories', require('./categories.routes'));
-router.use('/dishes', require('./dishes.routes'));
-
-module.exports = router;
