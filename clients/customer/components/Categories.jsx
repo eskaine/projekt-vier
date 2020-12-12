a89a35a11bb9baceed343215a6e15056e5fb9cac
jnsw <jns.w@icloud.com>
2020-12-12 09:57:20 +0800

    customer front

--- clients/customer/components/Categories.jsx
+++ clients/customer/components/Categories.jsx
@@ -1,53 +1,21 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { categoryData } from '../data/testData';
 
 function Categories({ openCategory }) {
-  const categoryData = [
-    {
-      name: 'Starters',
-      img_url:
-        'https://res.cloudinary.com/d7bdciwna5/image/upload/v1607397311/vier/cuxqLB9H2685fJoAAa3C.jpg',
-    },
-    {
-      name: 'Steak',
-      img_url:
-        'https://res.cloudinary.com/d7bdciwna5/image/upload/v1607397311/vier/J65f5UAB3TW5zpU5DlYU.jpg',
-    },
-    {
-      name: 'Combo',
-      img_url:
-        'https://res.cloudinary.com/d7bdciwna5/image/upload/v1607397311/vier/Qgyke2b3STK09wHWWs7R.jpg',
-    },
-    {
-      name: 'Seafood',
-      img_url:
-        'https://res.cloudinary.com/d7bdciwna5/image/upload/v1607397311/vier/J65f5UAB3TW5zpU5DlYU.jpg',
-    },
-    {
-      name: 'Beverages',
-      img_url:
-        'https://res.cloudinary.com/d7bdciwna5/image/upload/v1607397311/vier/sUDmgQV7ve4SgXOuNajL.jpg',
-    },
-    {
-      name: 'Pasta',
-      img_url:
-        'https://res.cloudinary.com/d7bdciwna5/image/upload/v1607397311/vier/uYLGylmtE8rQvPXlRSfu.jpg',
-    },
-  ];
-
   const categories = categoryData.map((category) => (
-    <div className="category-card" key={category.name}>
+    <div
+      className="category-card"
+      key={category.name}
+      onMouseUp={() => openCategory(category.name)}
+    >
       <img src={category.img_url} alt="" />
       <div className="category-info">
         <h2>{category.name}</h2>
         <p>7 items</p>
       </div>
-      <div
-        className="arrow-div"
-        onMouseUp={() => openCategory(category.name)}
-        onTouchEnd={() => openCategory(category.name)}
-      >
+      <div className="arrow-div">
         <FontAwesomeIcon icon={faChevronRight} className="fa-arrow" />
       </div>
     </div>
--- clients/customer/components/Dishes.jsx
+++ clients/customer/components/Dishes.jsx
@@ -2,52 +2,10 @@ import React, {useState} from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faPlus} from '@fortawesome/free-solid-svg-icons';
 import {faMinus} from '@fortawesome/free-solid-svg-icons';
+import { dishesData } from '../data/testData';
 
 function Dishes({selectedCategory, categoryHeaderPos}) {
-  const dishesData = [
-    {
-      name: 'Lobster & Crab Stuffed Mushrooms',
-      img_url:
-        'https://res.cloudinary.com/d7bdciwna5/image/upload/v1607397311/vier/cuxqLB9H2685fJoAAa3C.jpg',
-      price: 12,
-      description: 'Oysters on the half-shell baked with cheese & sprinkled with bacon bits',
-    },
-    {
-      name: 'Prawn & Mango Delight',
-      price: 12,
-      img_url:
-        'https://res.cloudinary.com/d7bdciwna5/image/upload/v1607397311/vier/Qgyke2b3STK09wHWWs7R.jpg',
-      description: 'Chicken wings marinated in Chef’s special seasoning & deep-fried to perfection',
-    },
-    {
-      name: 'Combo',
-      img_url:
-        'https://res.cloudinary.com/d7bdciwna5/image/upload/v1607397311/vier/sUDmgQV7ve4SgXOuNajL.jpg',
-      price: 12,
-      description: 'Chicken wings marinated in Chef’s special seasoning & deep-fried to perfection',
-    },
-    {
-      name: 'Seafood',
-      price: 5,
-      img_url:
-        'https://res.cloudinary.com/d7bdciwna5/image/upload/v1607397311/vier/uYLGylmtE8rQvPXlRSfu.jpg',
-      description: 'Chicken wings marinated in Chef’s special seasoning & deep-fried to perfection',
-    },
-    {
-      name: 'Beverages',
-      price: 15,
-      img_url:
-        'https://res.cloudinary.com/d7bdciwna5/image/upload/v1607397311/vier/J65f5UAB3TW5zpU5DlYU.jpg',
-      description: 'Chicken wings marinated in Chef’s special seasoning & deep-fried to perfection',
-    },
-    {
-      name: 'Pasta',
-      price: 12,
-      img_url:
-        'https://res.cloudinary.com/d7bdciwna5/image/upload/v1607397311/vier/uYLGylmtE8rQvPXlRSfu.jpg',
-      description: 'Chicken wings marinated in Chef’s special seasoning & deep-fried to perfection',
-    },
-  ];
+
 
   const [selectedDish, setSelectedDish] = useState('');
 
